bad2708fea002e26aa8352c468fccf882af59561
Owner <sandifee@email.sc.edu>
2015-11-29 21:46:58 -0500

    Initial commit

---
+++ app/src/main/java/edu/sc/myapplication/FPBodyBuildingFragment.java
@@ -0,0 +1,36 @@
+package edu.sc.myapplication;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FPBodyBuildingFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_fp__body__building, container, false);
+
+
+        Button btn = (Button) v.findViewById(R.id.backBodyBuilding);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FitnessProgramFragment fragment1 = new FitnessProgramFragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment1);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+        // Inflate the layout for this fragment
+        return v;
+    }
+}
